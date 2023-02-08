21c8c7e5ac6e4e5144d10665d9609a608818e692
Ajay Parmar <parmarajay13998@gmail.com>
2023-02-08 11:55:53 +0530

    shop and categories

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Elements } from '@stripe/react-stripe-js';
+// import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
 import { store, persistor } from './store/store';
-import { stripePromise } from './utils/stripe/stripe.utils';
 import * as registerServiceWorker from './serviceWorkerRegistration';
-
+// import { stripePromise } from '../src/utils/stripe/stripe.utils'
 const rootElement = document.getElementById('root');
 
 render(
@@ -17,9 +16,9 @@ render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <Elements stripe={stripePromise}>
+          {/* <Elements stripe={stripePromise}> */}
             <App />
-          </Elements>
+          {/* </Elements> */}
         </BrowserRouter>
       </PersistGate>
     </Provider>
